47a4e4a209222e6728ec2442ca5691b491cba6ef
codayomide <smokayomide@gmail.com>
2022-08-14 10:14:07 +0100

    commit on 'main'

---
+++ script.js
@@ -0,0 +1,9 @@
+let me = {
+    name: "Ayomide Adegoke",
+    height: "5ft 2in",
+    country: "Nigeria"
+};
+
+alert(`My name is ${me.name}`)
+alert(`I am ${me.height} tall`)
+alert(`I am from ${me.country}`)
